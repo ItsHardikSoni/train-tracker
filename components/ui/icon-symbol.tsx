a3bb2b35f9c6e70c2c683c5dda95a426b4f53a5e
hardiksoni019 <hardiksoni019@gmail.com>
2026-07-01 11:38:30 +0000

    aiew all icons

--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -1,18 +1,12 @@
-// Fallback for using MaterialIcons on Android and web.
-
+import { useThemeColor } from '@/hooks/use-theme-color';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { SymbolWeight, SymbolViewProps } from 'expo-symbols';
 import { ComponentProps } from 'react';
-import { OpaqueColorValue, type StyleProp, type TextStyle } from 'react-native';
+import { type StyleProp, type TextStyle } from 'react-native';
 
 type IconMapping = Record<SymbolViewProps['name'], ComponentProps<typeof MaterialIcons>['name']>;
 type IconSymbolName = keyof typeof MAPPING;
 
-/**
- * Add your SF Symbols to Material Icons mappings here.
- * - see Material Icons in the [Icons Directory](https://icons.expo.fyi).
- * - see SF Symbols in the [SF Symbols](https://developer.apple.com/sf-symbols/) app.
- */
 const MAPPING = {
   'house.fill': 'home',
   'dot.radiowaves.up.forward': 'radar',
@@ -21,13 +15,18 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'line.horizontal.3': 'menu',
+  'magnifyingglass': 'search',
+  'text.book.closed': 'book',
+  'map': 'map',
+  'arrow.counterclockwise': 'refresh',
+  'bell': 'notifications',
+  'figure.walk.motion': 'directions-walk',
+  'info.circle': 'info',
+  'doc.text': 'description',
+  'shield': 'security',
 } as IconMapping;
 
-/**
- * An icon component that uses native SF Symbols on iOS, and Material Icons on Android and web.
- * This ensures a consistent look across platforms, and optimal resource usage.
- * Icon `name`s are based on SF Symbols and require manual mapping to Material Icons.
- */
 export function IconSymbol({
   name,
   size = 24,
@@ -36,9 +35,11 @@ export function IconSymbol({
 }: {
   name: IconSymbolName;
   size?: number;
-  color: string | OpaqueColorValue;
+  color?: string;
   style?: StyleProp<TextStyle>;
   weight?: SymbolWeight;
 }) {
-  return <MaterialIcons color={color} size={size} name={MAPPING[name]} style={style} />;
+  const themeColor = useThemeColor({ light: color, dark: color }, 'icon');
+
+  return <MaterialIcons color={themeColor} size={size} name={MAPPING[name]} style={style} />;
 }
