19a6b66f8707aed325ccdefeec29a829a76ba5e0
ItsHardikSoni <hardiksoni019@gmail.com>
2026-07-04 19:26:49 +0530

    fix: clean up unused code, disable react compiler, align deps
    Remove unused useColorScheme import and related variable from tab layout
    Remove extra blank line at top of tabs layout file
    Disable react compiler experimental feature in app.json
    Align expo and related packages to ~54.x compatible ranges

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,9 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-
 import { TabBarIcon } from '@/components/navigation/tab-bar-icon';
 import { AppColors } from '@/constants/colors';
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
   return (
     <Tabs
