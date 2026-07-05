03ff4ed217351be1d73537996e3f91bd9aaa6b4d
ItsHardikSoni <hardiksoni019@gmail.com>
2026-07-06 00:16:12 +0530

    solve error

--- api/apiConfig.ts
+++ api/apiConfig.ts
@@ -1,13 +1,10 @@
 import Constants from "expo-constants";
 
 // Check if the environment variables are defined, if not, provide default values or throw an error.
-const API_KEY = Constants.expoConfig?.extra?.env?.API_KEY;
-const API_HOST = Constants.expoConfig?.extra?.env?.API_HOST;
-const PNR_API_HOST = Constants.expoConfig?.extra?.env?.PNR_API_HOST;
-const TRAIN_ROUTE_API_HOST = Constants.expoConfig?.extra?.env?.TRAIN_ROUTE_API_HOST;
+const API_KEY = Constants.expoConfig?.extra?.env?.API_KEY || "demo_api_key";
+const API_HOST = Constants.expoConfig?.extra?.env?.API_HOST || "demo_api_host";
+const PNR_API_HOST = Constants.expoConfig?.extra?.env?.PNR_API_HOST || "demo_pnr_host";
+const TRAIN_ROUTE_API_HOST = Constants.expoConfig?.extra?.env?.TRAIN_ROUTE_API_HOST || "demo_route_host";
 
-if (!API_KEY || !API_HOST || !PNR_API_HOST || !TRAIN_ROUTE_API_HOST) {
-  throw new Error("API keys and hosts are not defined in the environment variables.");
-}
+export { API_HOST, API_KEY, PNR_API_HOST, TRAIN_ROUTE_API_HOST };
 
-export { API_KEY, API_HOST, PNR_API_HOST, TRAIN_ROUTE_API_HOST };
